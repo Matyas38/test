f247afbb24c62ccec525a9b9617906edda28c046
Mathieu CANNEZZARO <mathieu.cannezzaro@gmail.com>
2022-09-19 22:12:07 +0200

    save

---
+++ upgrader.lua
@@ -0,0 +1,24 @@
+local x, y = term.getSize();
+local title = "Mayuas' CC Upgrader V1";
+
+term.clear();
+
+function printTitle()
+	local line = 2;
+	term.setCursorPos(1, line);
+	for i = 2, x, 1 do write("-") end
+	term.setCursorPos((x-title: len())/2, line+1);
+	print(title);
+	for i = 2, x, 1 do write("-") end
+end
+
+function main()
+	printTitle();
+
+	local content = http.get("https://raw.githubusercontent.com/Matyas38/test/master/upgrader.lua");
+	local file = fs.open("/upgrader.lua", "w");
+	file.write(content.readAll());
+	file.close();
+end
+
+main();
